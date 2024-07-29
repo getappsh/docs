79d2aa66a7224da6bdca32405889ccf9c4af1235
TPeled <tomermoshep@mail.tau.ac.il>
2024-07-29 10:54:29 +0300

    namechange

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'GetApp Site',
+  title: 'GetApp Developer Site',
   tagline: 'Managed Digital Assets',
   favicon: 'img/graynowords.png',
 
